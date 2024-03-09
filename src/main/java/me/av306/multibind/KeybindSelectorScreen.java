78b8697662c97a17f018fac93e9f6f361fc565d8
AV306 <brandonjoash@gmail.com>
2024-03-09 22:43:05 +0800

    we're on 1.20.4, we can put this here

--- src/main/java/me/av306/multibind/KeybindSelectorScreen.java
+++ src/main/java/me/av306/multibind/KeybindSelectorScreen.java
@@ -259,10 +259,10 @@ public class KeybindSelectorScreen extends Screen
     }
 
     // 1.20.2 onwards
-    /*@Override
+    @Override
     public void renderBackground( DrawContext context, int mouseX, int mouseY, float delta )
     {
         // Remove the darkened background
-        //super.renderBackground( context, mouseX, mouseY, delta );
-    }*/
+        super.renderBackground( context, mouseX, mouseY, delta );
+    }
 }
